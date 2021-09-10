9433e104a1ecabd6b40829750c816eec6bba500e
Tong Allan Zhao <allanzhao@tongs-air.attlocal.net>
2021-09-10 15:58:31 -0700

    implement unsplash api on search submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchBarSubmit(term) {
-    console.log(term);
+  state = { images: [] };
+  onSearchBarSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos/', {
+      params: {query: term},
+    });
+
+    this.setState({images: response.data.results});
   }
 
   render () {
     return (
       <div>
         <SearchBar onSubmit={this.onSearchBarSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     );
   }
